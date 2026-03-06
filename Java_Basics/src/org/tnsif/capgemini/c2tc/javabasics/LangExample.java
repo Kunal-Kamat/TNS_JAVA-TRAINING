4e10c18a9acf7b9819743295aeca03615ca3a4ab
Kunal-Kamat <kunal.kamat.24.k@gmail.com>
2026-03-07 00:57:57 +0530

    Predefined Pacakge Exmample

---
+++ Java_Basics/src/org/tnsif/capgemini/c2tc/javabasics/LangExample.java
@@ -0,0 +1,12 @@
+package org.tnsif.capgemini.c2tc.javabasics;
+
+public class LangExample {
+
+	public static void main(String[] args) {
+		String greet="Hello World";
+		System.out.println("Original String: "  + greet);
+		System.out.println("UpperCase: " + greet.toUpperCase());
+		System.out.println("LowerCase: " + greet.toLowerCase());
+	}
+
+}
