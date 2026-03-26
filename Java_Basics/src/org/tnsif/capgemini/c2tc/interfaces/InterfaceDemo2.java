6b5fa99d99ec537294be5b0c8fbc30f050a5e866
Kunal-Kamat <kunal.kamat.24.k@gmail.com>
2026-03-27 00:02:54 +0530

    Interface Demo

---
+++ Java_Basics/src/org/tnsif/capgemini/c2tc/interfaces/InterfaceDemo2.java
@@ -0,0 +1,60 @@
+package org.tnsif.capgemini.c2tc.interfaces;
+
+interface Vehicle{
+	void start();
+	void stop();
+
+	int Max_speed=120;
+}
+
+class Car5 implements Vehicle {
+    private String model;
+   
+	public Car5(String model) {
+		this.model = model;
+	}
+
+	@Override
+	public void start() {
+		System.out.println("Car " + model +" is staring ");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Car " + model +" is stoping ");
+	}
+}
+
+class Bike implements Vehicle{
+    private String type;
+   
+	public Bike(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public void start() {
+		System.out.println("Car " +type+" is staring ");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Car " + type+" is stoping ");
+	}
+}
+
+public class InterfaceDemo2 {
+
+	public static void main(String[] args) {
+		Vehicle car=new Car5("KIA");
+		Vehicle bike=new Bike("Royal Enfield"); 
+		
+		car.start();
+		car.stop();
+		
+		bike.start();
+		bike.stop();
+		
+		System.out.println("Maximun speed of vehicle is: " +  Vehicle.Max_speed );
+	}
+}
