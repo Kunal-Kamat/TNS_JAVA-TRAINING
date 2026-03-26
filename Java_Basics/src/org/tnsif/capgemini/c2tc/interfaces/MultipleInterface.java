cf6ccc8766a95e89cd61182d8a3a88811fbb660c
Kunal-Kamat <kunal.kamat.24.k@gmail.com>
2026-03-27 00:12:10 +0530

    Multiple Interface

---
+++ Java_Basics/src/org/tnsif/capgemini/c2tc/interfaces/MultipleInterface.java
@@ -0,0 +1,35 @@
+package org.tnsif.capgemini.c2tc.interfaces;
+
+interface Printer{
+	void print(String message);
+}
+
+interface Scanner{
+	void scan(String document);
+}
+
+class MultiFunction implements Printer,Scanner{
+
+	@Override
+	public void scan(String document) {
+		System.out.println("Scanning: " + document);
+		
+	}
+
+	@Override
+	public void print(String message) {
+		System.out.println("Printing: " +message );
+		
+	}
+	
+}
+public class MultipleInterface {
+
+	public static void main(String[] args) {
+		MultiFunction device=new MultiFunction();
+		device.scan("demo.pdf");
+		device.print("hello world");
+
+	}
+
+}
