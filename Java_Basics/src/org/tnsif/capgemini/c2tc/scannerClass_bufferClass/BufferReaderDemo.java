19981f96aef0ec2ab7c78d983d73abfa951e6b16
Kunal-Kamat <kunal.kamat.24.k@gmail.com>
2026-03-26 19:45:18 +0530

    Buffer Reader Demo

---
+++ Java_Basics/src/org/tnsif/capgemini/c2tc/scannerClass_bufferClass/BufferReaderDemo.java
@@ -0,0 +1,21 @@
+package org.tnsif.capgemini.c2tc.scannerClass_bufferClass;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferReaderDemo {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new FileReader("C:\\Users\\KUNAL\\Downloads\\demo.txt"));
+
+        String data = "";
+
+        while ((data = br.readLine()) != null) {
+            System.out.println(data);
+        }
+
+        br.close();
+    }
+}
